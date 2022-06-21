5cd4cbc7e9f60869eaa06879af57551e1daa327e
José Cuya <jmcuya@gmail.com>
2022-06-21 00:00:49 -0500

    Aprobación y detalle de aprobación

--- src/app/pages/main/operador/solicitud-detail/solicitud-detail.component.ts
+++ src/app/pages/main/operador/solicitud-detail/solicitud-detail.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./solicitud-detail.component.scss']
 })
 export class SolicitudDetailComponent implements OnInit {
+    load = false;
 
   constructor() { }
 
